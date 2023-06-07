e5ef20665f476654c3431b69ab37134a3c96b67a
WonkMont <133684901+WonkMont@users.noreply.github.com>
2023-06-07 09:00:13 -0700

    Start Menu V.1

---
+++ Assets/Scripts/StartMenuScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuScript : MonoBehaviour
+{
+    public void LoadCutscene()
+    {
+        SceneManager.LoadScene(1);
+    }
+   public void LoadLvlOne()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
