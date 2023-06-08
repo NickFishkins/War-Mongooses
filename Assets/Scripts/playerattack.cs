403c007c0967c39a68e415e7e08a001fba2e8aa3
ieguras <0510020@mlsd.org>
2023-06-08 09:26:25 -0700

    instantiating player attack script

---
+++ Assets/Scripts/playerattack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerattack : MonoBehaviour
+{
+    //this script will be on the player instantiating the prefab
+    [SerializeField] GameObject attackhitbox;
+    private GameObject enemykiller;
+    public GameObject player;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("E"))
+        {
+            enemykiller = Instantiate(attackhitbox) as GameObject;
+            enemykiller.transform.position = player.transform.position;
+            Invoke(nameof(yourmother), 1f);
+        }
+    }
+    void yourmother()
+    {
+        GameObject.Destroy(enemykiller);
+    }
+}
